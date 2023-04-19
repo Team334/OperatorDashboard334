26c80e4e8d1d4992cf8b9b6bec2effc889666f5c
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2023-04-18 21:32:25 -0400

    fixed some testing file issue

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:operator_dashboard_334/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const OperatorDashboard334());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
