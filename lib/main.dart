e86c151ea7a632f2c90b23fdf5030438beb4f895
Elvis Osmanov <gamerprotime777@gmail.com>
2023-04-19 17:58:53 -0400

    Added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,42 @@ class OperatorDashboard334 extends StatelessWidget {
         // AppBar takes a Text Widget in it's title parameter
         title: const Text('OperatorDashboard334'),
       ),
-      body: const Center(child: Text('Hello from 334!')),
+      body: const MyStatefulWidget(),
     ));
   }
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: null,
+            child: const Text('Disabled'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
